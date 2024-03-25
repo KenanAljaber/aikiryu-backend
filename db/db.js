eaf730cc6e5e4a34c4543ae401059173a4cb99d8
KenanAljaber <keno12333@hotmail.com>
2024-03-25 10:39:14 +0100

    modifier la conexion au base de donnes

--- db/db.js
+++ db/db.js
@@ -1,43 +1,39 @@
 const { Client } = require('pg');
 const TABLES_QUERIES = require('./tables');
 
-
-const client = new Client({
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-});
+let client = null;
 
 const CREATE_TABLE_QUERY = TABLES_QUERIES.CONTACT + TABLES_QUERIES.REQUEST + TABLES_QUERIES.ADMIN + TABLES_QUERIES.EVENT + TABLES_QUERIES.EVENT_SCHEDULE;
 
 module.exports = {
     connect: async () => {
-        let connected = false;
-        let retries = 5;
-        while (!connected && retries > 0) {
-
+        let retries = 5; // Maximum number of retry attempts
+        while (retries > 0) {
             try {
+                client = new Client({
+                    user: process.env.DB_USERNAME,
+                    password: process.env.DB_PASSWORD,
+                    host: process.env.DB_HOST,
+                    database: process.env.DB_NAME,
+                    port: process.env.DB_PORT,
+                });
                 await client.connect();
                 console.log('[+] Connected to PostgreSQL');
                 const result = await client.query(CREATE_TABLE_QUERY);
-
-                connected = true;
-
                 if (result) {
                     console.log('[+] Tables created successfully');
                 }
-
+                return; // Exit the loop if connection and table creation are successful
             } catch (error) {
                 retries--;
                 console.log(`[!] Could not connect to PostgreSQL:`, error);
                 console.log(`[!] Retries left: ${retries}`);
                 if (retries === 0) {
                     console.log('[!] Max retries reached, exiting...');
-                    process.exit(1);
+                    process.exit(1); // Exit the process if maximum retries reached
                 }
-                await new Promise(resolve => setTimeout(resolve, 4000));
+                await client.end();
+                await new Promise(resolve => setTimeout(resolve, 5000)); // Wait before retrying
             }
         }
     },
@@ -49,7 +45,4 @@ module.exports = {
             console.log(`[!] Could not execute query:`, error);
         }
     }
-
-
-
-}
+};
